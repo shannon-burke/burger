4632e08b4faf20ee4f2a373dc36cfedf19437321
Shannon <shannonburke.art@gmail.com>
2019-10-19 20:25:50 -0500

    required things and added app commands in server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 3306;
-
 var app = express();
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use("/", routes);
+
+app.listen(PORT);
